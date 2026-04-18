736f634c8f15a6342b292e66cf0f02d28cafbc7a
KimKihoon <leonardkim1209@gmail.com>
2026-04-18 10:12:29 +0900

    add: VertexLayouts shader in/outputs

--- KraftonEngine/Shaders/Common/VertexLayouts.hlsl
+++ KraftonEngine/Shaders/Common/VertexLayouts.hlsl
@@ -6,7 +6,6 @@
 // ============================================================
 
 // FVertex (Position + Color)
-// 사용: Primitive, Editor, Gizmo, Outline, Line
 struct VS_Input_PC
 {
     float3 position : POSITION;
@@ -14,7 +13,6 @@ struct VS_Input_PC
 };
 
 // FVertexPNCT (Position + Normal + Color + TexCoord)
-// 사용: StaticMesh, OutlinePNCT
 struct VS_Input_PNCT
 {
     float3 position : POSITION;
@@ -23,15 +21,25 @@ struct VS_Input_PNCT
     float2 texcoord : TEXTCOORD;
 };
 
+// FVertexPNCT_T (Position + Normal + Color + TexCoord + Tangent)
+// 라이팅 및 노멀 매핑용 입력
+struct VS_Input_Lighting
+{
+    float3 position : POSITION;
+    float3 normal   : NORMAL;
+    float4 color    : COLOR;
+    float2 texcoord : TEXCOORD0;
+    float4 tangent  : TANGENT;
+};
+
 // FTextureVertex (Position + TexCoord)
-// 사용: Font, SubUV, OverlayFont
 struct VS_Input_PT
 {
     float3 position : POSITION;
     float2 texcoord : TEXCOORD;
 };
 
-// Position only (Outline primitive expansion)
+// Position only
 struct VS_Input_P
 {
     float3 position : POSITION;
@@ -64,14 +72,34 @@ struct PS_Input_Full
     float2 texcoord : TEXTCOORD;
 };
 
-// SV_POSITION + UV (PostProcess: HeightFog, Outline, SceneDepth)
+// SV_POSITION + WorldPos + Normal + UV + Tangent + Color (Uber Shader)
+struct PS_Input_Lighting
+{
+    float4 position  : SV_POSITION;
+    float3 worldPos  : TEXCOORD0;
+    float3 normal    : TEXCOORD1;
+    float2 uv        : TEXCOORD2;
+    float4 tangent   : TEXCOORD3;
+    float4 color     : COLOR;
+};
+
+// SV_POSITION + WorldPos + Normal + Color (Decal)
+struct PS_Input_Decal
+{
+    float4 position : SV_POSITION;
+    float3 worldPos : TEXCOORD0;
+    float3 normal : TEXCOORD1;
+    float4 color : COLOR;
+};
+
+// SV_POSITION + UV (PostProcess)
 struct PS_Input_UV
 {
     float4 position : SV_POSITION;
     float2 uv : TEXCOORD0;
 };
 
-// SV_POSITION only (Outline)
+// SV_POSITION only
 struct PS_Input_PosOnly
 {
     float4 position : SV_POSITION;
@@ -85,12 +113,16 @@ struct PS_Input_ColorWorld
     float3 worldPos : TEXCOORD0;
 };
 
-struct PS_Input_Decal
+// ============================================================
+// PS Output (픽셀 쉐이더 출력 구조체)
+// ============================================================
+
+// G-Buffer Output (BaseDraw Pass)
+struct PS_Output_GBuffer
 {
-    float4 position : SV_POSITION;
-    float3 worldPos : TEXCOORD0;
-    float3 normal : TEXCOORD1;
-    float4 color : COLOR;
+    float4 BaseColor : SV_Target0;
+    float4 Normal    : SV_Target1;
+    float4 GouraudL  : SV_Target2; // Per-vertex light for Gouraud shading
 };
 
 #endif // VERTEX_LAYOUTS_HLSL
