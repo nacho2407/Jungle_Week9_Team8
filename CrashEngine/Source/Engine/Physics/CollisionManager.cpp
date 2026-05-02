2293069969045858def3724f8d28788c536f16e7
kwonhyeonsoo-goo <kkhhss4035@gmail.com>
2026-05-03 02:02:32 +0900

    add. OverlayInfo

--- CrashEngine/Source/Engine/Physics/CollisionManager.cpp
+++ CrashEngine/Source/Engine/Physics/CollisionManager.cpp
@@ -210,6 +210,9 @@ void FCollisionManager::TickCollision(float DeltaTime, FScene* Scene)
         UE_LOG(Console, Info, "Collision %s and Collision %s Begin Collision!", 
 			Pair.A->GetFName().ToString().c_str(), Pair.B->GetFName().ToString().c_str());
 
+		Pair.A->AddOverlapInfo(Pair.B);
+        Pair.B->AddOverlapInfo(Pair.A);
+
         // 나중에 BeginOverlap 이벤트를 구현한다면 
         // Pair.A->OnComponentBeginOverlap(Pair.B);
         // Pair.B->OnComponentBeginOverlap(Pair.A);
@@ -219,6 +222,9 @@ void FCollisionManager::TickCollision(float DeltaTime, FScene* Scene)
     {
         UE_LOG(Console, Info, "Collision %s and Collision %s End Collision!", 
 			Pair.A->GetFName().ToString().c_str(), Pair.B->GetFName().ToString().c_str());
+
+		Pair.A->RemoveOverlapInfo(Pair.B);
+        Pair.B->RemoveOverlapInfo(Pair.A);
 
         // 나중에 EndOverlap 이벤트를 구현한다면 
         // Pair.A->OnComponentEndOverlap(Pair.B);
